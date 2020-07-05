44c10efb533ce0bc82b5a1a71a2f4baa29d1144e
yonatan shmuel <2017yonatan@gmail.com>
2020-07-05 23:43:47 +0300

    first-try

---
+++ חדשות - האתר החרדי המוביל בעולם - כיכר השבת_files/bd(1).php
@@ -0,0 +1,29 @@
+document.write('<div class="sps" style="float:right;width: 300px;overflow: hidden;height: 151px;box-sizing: border-box;padding:7px">');
+document.write(		'<div style="float: right;width: 285px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/fs/m4eg9240__w285h109q95.jpg" alt="השמירה של שר התורה: קרן &#039;והסירותי&#039; וכתב הההגנה" border="0" style="display: block" width="300" height="109" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">השמירה של שר התורה: קרן \'והסירותי\' וכתב הההגנה</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2269" target="_blank">');
+document.write(			'<div  style="width:300px; height: 151px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div><div style="clear:both;"></div>');
+document.write('<div class="sps" style="float:right;width: 300px;overflow: hidden;height: 151px;box-sizing: border-box;padding:7px">');
+document.write(		'<div style="float: right;width: 285px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/ql/35ls7uv2__w285h109q95.jpg" alt="מחפשים דירה לכניסה מיידית? אכלוס עד 2021 במרכז י-ם" border="0" style="display: block" width="300" height="109" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">מחפשים דירה לכניסה מיידית? אכלוס עד 2021 במרכז י-ם</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2263" target="_blank">');
+document.write(			'<div  style="width:300px; height: 151px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div><div style="clear:both;"></div>');
+
---
+++ חדשות - האתר החרדי המוביל בעולם - כיכר השבת_files/bd.php
@@ -0,0 +1,85 @@
+document.write('<div class="sps" style="float:right;width: 216px;overflow: hidden;height: 160px;box-sizing: border-box;padding:5px">');
+document.write(		'<div style="float: right;width: 205px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/fs/m4eg9240__w205h120q95.jpg" alt="השמירה של שר התורה: קרן &#039;והסירותי&#039; וכתב הההגנה" border="0" style="display: block" width="216" height="120" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">השמירה של שר התורה: קרן \'והסירותי\' וכתב הההגנה</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2269" target="_blank">');
+document.write(			'<div  style="width:216px; height: 160px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div>');
+document.write('<div class="sps" style="float:right;width: 216px;overflow: hidden;height: 160px;box-sizing: border-box;padding:5px">');
+document.write(		'<div style="float: right;width: 205px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/addonsmgr/tk/d7ui79bm__w205h120q95.jpg" alt="תואר ראשון בקמפוס שטראוס מבית המכללה האקדמית הדסה" border="0" style="display: block" width="216" height="120" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">תואר ראשון בקמפוס שטראוס מבית המכללה האקדמית הדסה</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2225" target="_blank">');
+document.write(			'<div  style="width:216px; height: 160px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div>');
+document.write('<div class="sps" style="float:right;width: 216px;overflow: hidden;height: 160px;box-sizing: border-box;padding:5px">');
+document.write(		'<div style="float: right;width: 205px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/xa/rximrbmw__w205h120q95.jpeg" alt="הפקת אירועי קורונה לכל כיס: בשמחה שלך!" border="0" style="display: block" width="216" height="120" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">הפקת אירועי קורונה לכל כיס: בשמחה שלך!</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2271" target="_blank">');
+document.write(			'<div  style="width:216px; height: 160px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div>');
+document.write('<div class="sps" style="clear:both;float:right;width: 216px;overflow: hidden;height: 160px;box-sizing: border-box;padding:5px">');
+document.write(		'<div style="float: right;width: 205px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/fi/kv1hctt6__w205h120q95.jpg" alt="75% הנחה וייעוץ טלפוני לעתידך האקדמי!" border="0" style="display: block" width="216" height="120" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">75% הנחה וייעוץ טלפוני לעתידך האקדמי!</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2243" target="_blank">');
+document.write(			'<div  style="width:216px; height: 160px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div>');
+document.write('<div class="sps" style="float:right;width: 216px;overflow: hidden;height: 160px;box-sizing: border-box;padding:5px">');
+document.write(		'<div style="float: right;width: 205px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/sw/1c23pr7v__w205h120q95.png" alt="מחתנים בקרוב? זה הצעד הראשון שלך בשביל הזוג הצעיר" border="0" style="display: block" width="216" height="120" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">מחתנים בקרוב? זה הצעד הראשון שלך בשביל הזוג הצעיר</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2261" target="_blank">');
+document.write(			'<div  style="width:216px; height: 160px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div>');
+document.write('<div class="sps" style="float:right;width: 216px;overflow: hidden;height: 160px;box-sizing: border-box;padding:5px">');
+document.write(		'<div style="float: right;width: 205px;overflow: hidden;position:relative" >');
+document.write(			'<img src="https://s1.kikar.co.il/th/data/auto/nadm/bn/agf2ps8g__w205h120q95.png" alt="גיקטיים הופך לאתר בינלאומי וגם אתם יכולים להשקיע בו" border="0" style="display: block" width="216" height="120" />');
+/*ad*/
+document.write(			'<div style="    position: absolute;    bottom: 0;    left: 0;    background-color: #9e9e9ea6;"><span style="color:#fff;font-size:smaller;">&nbsp;מקודם&nbsp;</span>');
+document.write(			'</div>');
+document.write(		'</div>');
+document.write(		'<div style="    float: right;padding: 1px 0px;box-sizing: border-box;overflow: hidden;" >');
+document.write(			'<div style="color: #000;font-size: 14px;line-height: 16px;font-weight: 700;max-height: 39px;overflow:hidden;">גיקטיים הופך לאתר בינלאומי וגם אתם יכולים להשקיע בו</div>');
+document.write(		'</div>');
+document.write(		'<a href="https://www.kikar.co.il/bdclk.php?id=2230" target="_blank">');
+document.write(			'<div  style="width:216px; height: 160px;position:relative;"></div>');
+document.write(		'</a>');
+document.write('</div>');
+
